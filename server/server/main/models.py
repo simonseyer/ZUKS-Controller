6ef039b307e41c30b5f76177a92c237983b7ab5b
Simon Seyer <seyer@redpeppix.de>
2014-11-24 18:21:23 +0100

    Integrated leaflet

--- server/server/main/models.py
+++ server/server/main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Location(models.Model):
-  latitude = models.DecimalField(decimal_places=8, max_digits=11, required=True)
-  longitude = models.DecimalField(decimal_places=8, max_digits=11, required=True)
+  latitude = models.DecimalField(decimal_places=8, max_digits=11)
+  longitude = models.DecimalField(decimal_places=8, max_digits=11)
 
 class Volunteer(models.Model):
-  first_name = models.CharField(max_length=30, required=True)
-  last_name = models.CharField(max_length=30, required=True)
-  location = models.OneToOneField(Location, required=True)
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  location = models.OneToOneField(Location)
 
