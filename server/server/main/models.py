154dfb43c260a918d0c017678c255a72aea48f2d
Simon Seyer <seyer@redpeppix.de>
2014-12-01 23:26:25 +0100

    Updated size of location fields in the server model

--- server/server/main/models.py
+++ server/server/main/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Location(models.Model):
-  latitude = models.DecimalField(decimal_places=8, max_digits=11)
-  longitude = models.DecimalField(decimal_places=8, max_digits=11)
+  latitude = models.DecimalField(decimal_places=18, max_digits=20)
+  longitude = models.DecimalField(decimal_places=18, max_digits=20)
 
 class Volunteer(models.Model):
   first_name = models.CharField(max_length=30)
