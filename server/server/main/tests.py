63fe368f8db4d5e2c9771827a3cd639dbf467bf2
Simon Seyer <seyer@redpeppix.de>
2014-12-08 17:02:05 +0100

    Moved tests in correct file nr 2

---
+++ server/server/main/tests.py
@@ -0,0 +1,54 @@
+from django.test import TestCase
+
+from .message_bus import MessageBus
+
+class MessageBusTestCase(TestCase):
+  def setUp(self):
+    self.message_bus = MessageBus()
+    self.eventKey = None
+    self.eventObject = None
+
+  def test_simple_key(self):
+    self.message_bus.addHandler('test', self.simple_key_handler)
+
+    self.message_bus('test', None)
+
+    self.assertEqual(self.eventKey, 'test')
+
+  def simple_key_handler(self, key, value):
+    self.eventKey = key
+    self.eventObject = value
+
+  def test_regex_key(self):
+    self.message_bus.addHandler('test_.*', self.simple_key_handler)
+
+    self.message_bus('test_fu', None)
+
+    self.assertEqual(self.eventKey, 'test_fu')
+
+  def test_value_parameter(self):
+    self.message_bus.addHandler('test', self.simple_key_handler)
+
+    self.message_bus('test', self)
+
+    self.assertEqual(self.eventObject, self)
+
+  def test_remove_handler(self):
+    self.message_bus.addHandler('test', self.simple_key_handler)
+    self.message_bus.removeHandler('test', self.simple_key_handler)
+
+    self.message_bus('test', None)
+
+    self.assertEqual(self.eventKey, None)
+
+  def test_remove_handler_func(self):
+    self.message_bus.addHandler('test', self.simple_key_handler)
+    self.message_bus.addHandler('fu', self.simple_key_handler)
+
+    self.message_bus.removeHandler(None, self.simple_key_handler)
+
+    self.message_bus('test', None)
+    self.assertEqual(self.eventKey, None)
+
+    self.message_bus('fu', None)
+    self.assertEqual(self.eventKey, None)
