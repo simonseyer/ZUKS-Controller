cf8ee24ff51b989dd4ec79916bc94621705c22b9
Simon Seyer <seyer@redpeppix.de>
2014-12-11 17:49:25 +0100

    Added web notifier tests

--- server/server/main/tests.py
+++ server/server/main/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 
 from server.main.event_bus import EventBus
+from server.main.web_notifier import WebsocketService, WebNotifier
+
+import json
+from decimal import Decimal
 
 class EventBusTestCase(TestCase):
   def setUp(self):
@@ -52,3 +56,54 @@ class EventBusTestCase(TestCase):
 
     self.event_bus('fu', None)
     self.assertEqual(self.eventKey, None)
+
+class WebNotifierTestCase(TestCase):
+
+  class WebsocketServiceMock(WebsocketService):
+
+    def __init__(self, callback):
+      self.callback = callback
+
+    def send(self, data):
+      self.callback(data)
+
+  def setUp(self):
+    self.event_bus = EventBus(False)
+    self.websocket_service = WebNotifierTestCase.WebsocketServiceMock(self.setData)
+    self.web_notifier = WebNotifier(self.event_bus, self.websocket_service)
+      
+  def setData(self, data):
+    self.data = data
+
+  def test_empty_message(self):
+    self.event_bus('test', None)
+
+    dataObject = json.loads(self.data)
+
+    self.assertEqual(dataObject['event'], 'test')
+    self.assertEqual(dataObject['data'], None)
+
+  def test_simple_message(self):
+    self.event_bus('test', 'test-data')
+
+    dataObject = json.loads(self.data)
+
+    self.assertEqual(dataObject['event'], 'test')
+    self.assertEqual(dataObject['data'], 'test-data')
+
+  def test_complex_message(self):
+    test_data = {
+      'a' : {
+        'b' : 'nested'
+      },
+      'b' : 1,
+      'c' : Decimal(1.5)
+    }
+
+    self.event_bus('test', test_data)
+
+    dataObject = json.loads(self.data)['data']
+
+    self.assertEqual(dataObject['a']['b'], 'nested')
+    self.assertEqual(dataObject['b'], 1)
+    self.assertEqual(dataObject['c'], 1.5)
