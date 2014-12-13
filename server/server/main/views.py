986c0fe1ad9056b58c4797371458ce18accd7990
Simon Seyer <seyer@redpeppix.de>
2014-12-13 19:44:55 +0100

    Created generic event view set

--- server/server/main/views.py
+++ server/server/main/views.py
@@ -8,27 +8,51 @@ default_event_bus = EventBus()
 logger = EventBusLogger(default_event_bus)
 web_notifier = WebNotifier(default_event_bus)
 
-class VolunteerViewSet(viewsets.ModelViewSet):
+class EventViewSet(viewsets.ModelViewSet):
+  '''
+  ModelViewSet that fires events, when the model
+  is changed. 
+
+  The events are published to the global 
+  default_event_bus.
+  '''
+
+  def create(self, request):
+    response = super().create(request)
+    self.fire(response, 'create')
+    return response
+
+  def update(self, request, pk=None):
+    response = super().update(request)
+    self.fire(response, 'update')
+    return response
+
+  def partial_update(self, request, pk=None):
+    response = super().partial_update(request)
+    self.fire(response, 'update')
+    return response
+
+  def fire(self, response, event):
+    '''
+    Creates a new event and publishes that event
+    to the global default_event_bus.
+
+    As prefix for the event name the class variable
+    event_key is used.
+
+    Keyword arguments:
+    response -- the reponse after of the crud function
+    event -- the event name, used to create the event_key
+    '''
+    if status.is_success(response.status_code):
+      key = "%s_%s" % (self.__class__.event_key, event)
+      default_event_bus(key, response.data)
+
+class VolunteerViewSet(EventViewSet):
   """
-  API endpoint that allows volunteers to be viewed or edited.
+  API endpoint for volunteers
   """
   queryset = Volunteer.objects.all()
   serializer_class = VolunteerSerializer
+  event_key = "volunteer"
 
-  def create(self, request):
-    result = super().create(request)
-    if status.is_success(result.status_code):
-      default_event_bus('volunteer_create', result.data)
-    return result
-
-  def update(self, request, pk=None):
-    result = super().update(request)
-    if status.is_success(result.status_code):
-      default_event_bus('volunteer_update', result.data)
-    return result
-
-  def partial_update(self, request, pk=None):
-    result = super().partial_update(request)
-    if status.is_success(result.status_code):
-      default_event_bus('volunteer_update', result.data)
-    return result
