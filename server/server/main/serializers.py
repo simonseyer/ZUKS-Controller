aa2fb968e6d53bd519b0ff4138964534bf44ffdd
Simon Seyer <seyer@redpeppix.de>
2014-12-15 14:47:31 +0100

    Configured volunteer serialiser for group reference

--- server/server/main/serializers.py
+++ server/server/main/serializers.py
@@ -2,11 +2,12 @@ from server.main.models import Volunteer, VolunteerGroup
 from rest_framework import serializers
 
 
-class VolunteerSerializer(serializers.HyperlinkedModelSerializer):
+class VolunteerSerializer(serializers.ModelSerializer):
+    group = serializers.PrimaryKeyRelatedField(queryset=VolunteerGroup.objects.all())
     class Meta:
         model = Volunteer
         depth = 1
 
-class VolunteerGroupSerializer(serializers.HyperlinkedModelSerializer):
+class VolunteerGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = VolunteerGroup
