2ccb141b940045cf127b650eccd509675050fe73
Simon Seyer <simon.seyer@gmail.com>
2015-01-05 15:44:33 +0100

    Added create method for nested location

--- server/server/main/serializers.py
+++ server/server/main/serializers.py
@@ -7,9 +7,16 @@ class LocationSerializer(serializers.ModelSerializer):
 
 class VolunteerSerializer(serializers.ModelSerializer):
     group = serializers.PrimaryKeyRelatedField(queryset=VolunteerGroup.objects.all())
+    location = LocationSerializer()
     class Meta:
         model = Volunteer
         depth = 1
+
+    def create(self, validated_data):
+        location_data = validated_data.pop('location')
+        location = Location.objects.create(**location_data)
+        volunteer = Volunteer.objects.create(location=location, **validated_data)
+        return volunteer
 
 class VolunteerGroupSerializer(serializers.ModelSerializer):
     class Meta:
