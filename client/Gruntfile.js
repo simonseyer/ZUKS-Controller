ac9d1c3b256cfddf20dc0a9ccd0f942f99251947
Simon Seyer <simon.seyer@gmail.com>
2015-03-14 12:39:50 +0100

    Updated grunt file to watch the new index files

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     watch: {
       code: {
-        files: ['components/**', 'index.html'],
+        files: ['components/**', 'client.html', 'controller.html', 'demo.html'],
         options: {
           livereload: true
         }
