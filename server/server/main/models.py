9d1547d837e625a09d03013e65d8664859b72e63
Simon Seyer <simon.seyer@gmail.com>
2015-04-30 00:32:59 +0200

    Made volunteer name unique
    WARNING: potentially breaks existing databases. Please delete the
    db.sqlite3 and re-initialize the database (migrate & loaddata as
    described in the README)

--- server/server/main/models.py
+++ server/server/main/models.py
@@ -25,6 +25,7 @@ class Volunteer(models.Model):
 
   class Meta:
     ordering = ['first_name', 'last_name']
+    unique_together = ("first_name", "last_name")
 
   def __str__(self):
     return "%s %s" % (self.first_name, self.last_name)
